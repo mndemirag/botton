a4bea2bc7ba17b27f009badc74abfef1e64e329e
Melodi Demirag <mndemirag@gmail.com>
2019-05-08 13:19:52 +0000

    Connect LCD and write from the button click

--- deploy.py
+++ deploy.py
@@ -1,4 +1,5 @@
 from button import Button
+from lcd import LCD
 
 DEPLOY_PORT = 22
 
@@ -6,13 +7,15 @@ DEPLOY_PORT = 22
 class DeployModule(object):
     def __init__(self):
         self.deploy_button = Button(DEPLOY_PORT, self.pressed)
+        self.lcd = LCD()
 
     def pressed(self, new_value):
         if new_value:
             print('Deploy!!')
+            self.lcd.write('TJENA!')
 
     def process(self):
         self.deploy_button.read_input()
 
     def destroy(self):
-        pass
+        self.lcd.destroy()
