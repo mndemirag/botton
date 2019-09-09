5ef1f98573177d72426be3f05a038af42bb6e9e4
Pierre Ingmansson <pierrei@spotify.com>
2019-09-09 13:36:26 +0200

    Hashing the auth tag

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+import hashlib
 import requests
 
 BOB_BASE_URL = 'http://192.168.0.1:5050'
@@ -5,21 +6,26 @@ BOB_BASE_URL = 'http://192.168.0.1:5050'
 
 class BobApi(object):
     def __init__(self, tag):
-        self.tag = tag
+        h = hashlib.sha256()
+        h.update(tag)
+        self.auth = h.hexdigest()
+
+    def auth_header(self):
+        return {'Authorization': self.auth}
 
     def get_repos_and_user_name(self):
         url = '{base}/repos'.format(base=BOB_BASE_URL)
-        return self.handle_response(requests.get(url, headers={'Authorization': self.tag}))
+        return self.handle_response(requests.get(url, headers=self.auth_header()))
 
     def get_pull_requests(self, repo):
         url = '{base}/repos/{name}/pulls'.format(base=BOB_BASE_URL, name=repo)
-        return self.handle_response(requests.get(url, headers={'Authorization': self.tag})).get('pull_requests')
+        return self.handle_response(requests.get(url, headers=self.auth_header())).get('pull_requests')
 
     def deploy(self, repo_name, pr_number):
         url = '{base}/repos/{repo_name}/pulls/{pr_number}/merge'.format(base=BOB_BASE_URL,
                                                                         repo_name=repo_name,
                                                                         pr_number=pr_number)
-        return self.handle_response(requests.put(url, headers={'Authorization': self.tag}))
+        return self.handle_response(requests.put(url, headers=self.auth_header()))
 
     def handle_response(self, response):
         if response.status_code == 200:
