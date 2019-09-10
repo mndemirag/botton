91305a97378d1ac4c3ba1f68072891cbb376bc47
Pierre Ingmansson <pierrei@spotify.com>
2019-09-10 14:46:36 +0200

    More stable rfid processing

--- rfid.py
+++ rfid.py
@@ -10,11 +10,14 @@ class RFID(object):
         # Scan for cards
         (status, TagType) = self.rfid.MFRC522_Request(self.rfid.PICC_REQIDL)
 
-        # Get the UID of the card
-        (status, uid_array) = self.rfid.MFRC522_Anticoll()
-        uid = ''.join(map(lambda x: str(x), uid_array))
-
-        # If we have the UID, continue
         if status == self.rfid.MI_OK:
-            print('Got uid: ' + uid)
-            self.callback(uid)
+            print "Card detected"
+
+            # Get the UID of the card
+            (status, uid_array) = self.rfid.MFRC522_Anticoll()
+
+            if status == self.rfid.MI_OK:
+                uid = ''.join(map(lambda x: str(x), uid_array))
+
+                print('Got uid: ' + uid)
+                self.callback(uid)
